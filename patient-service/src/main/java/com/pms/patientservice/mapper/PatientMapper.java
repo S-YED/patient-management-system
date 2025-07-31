d2e56d003a9c6e0145981fdb32e391920d2061f5
Syed Khaja Moinuddin <syedkhajamoinuddin293@gmail.com>
2025-07-31 23:04:35 +0530

    UPDATED PatientMapper Object

--- patient-service/src/main/java/com/pms/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pms/patientservice/mapper/PatientMapper.java
@@ -1,4 +1,16 @@
 package com.pms.patientservice.mapper;
 
+import com.pms.patientservice.dto.PatientResponseDTO;
+import com.pms.patientservice.model.Patient;
+
 public class PatientMapper {
+    public static PatientResponseDTO toDto (Patient patient) {
+        PatientResponseDTO patientDTO = new PatientResponseDTO();
+        patientDTO.setId(patient.getId().toString());
+        patientDTO.setName(patient.getName());
+        patientDTO.setAddress(patient.getAddress());
+        patientDTO.setEmail(patient.getEmail());
+        patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
+        return patientDTO;
+    }
 }
